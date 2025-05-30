84461a659769e25d2af20447220e614356add733
Nicolas Goulart <nicolasgoulart06@gmail.com>
2025-05-30 11:36:52 -0300

    Inicio

---
+++ CursoDeCSharp/CursoDeCSharp/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using CursoDeCSharp.Fundamentos;
+
+namespace CursoCSharp {
+    class Program {
+        static void Main(string[] args) {
+            var central = new CentralDeExercicios(new Dictionary<string, Action>() {
+                {"Primeiro Programa - Fundamentos", PrimeiroPrograma.Executar},
+            });
+
+            central.SelecionarEExecutar();
+        }
+    }
+}
